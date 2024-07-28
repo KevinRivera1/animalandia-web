5bd1f05d989a0bc4270628fb2c9e6ebbcf7431fc
Kevin Rivera <shimpo11.kr@gmail.com>
2024-07-28 14:16:58 -0500

    feat: Agregar componente de encabezado a la página web

---
+++ components/header.php
@@ -0,0 +1,29 @@
+<header class="header">
+    <div class="header__logo">
+        <img class="header__logo-image" src="../assets/img/logo.avif" alt="Logo de la empresa">
+    </div>
+    <div class="header__title">
+        <img class="header__title-gif" src="../assets/gifs/animalandia.gif">
+    </div>
+    <div class="header__menu_container">
+        <button class="header__menu-btn" id="menu-btn">
+            &#9776;
+        </button>
+    </div>
+    <nav class="header__nav" id="nav-menu">
+        <ul class="header__nav-list">
+            <li class="header__nav-item">
+                <a class="header__nav-link" href="../index.php">
+                    <img class="header__nav-icon" src="../assets/icons/inicio.svg" alt="Icono de búsqueda">
+                    Inicio
+                </a>
+            </li>
+            <li class="header__nav-item header__nav-item-acerca">
+                <a class="header__nav-link" href="#proteger-animales">
+                    <img class="header__nav-icon" src="../assets/icons/escudo.svg" alt="Icono de búsqueda">
+                    Protejamos
+                </a>
+            </li>
+        </ul>
+    </nav>
+</header>
