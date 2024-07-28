d0259e9e83482d32ce38b2b73168aa9ff9d373b7
Kevin Rivera <shimpo11.kr@gmail.com>
2024-07-28 14:17:16 -0500

    feat: Agregar componente de banner a la página principal

---
+++ components/banner.php
@@ -0,0 +1,7 @@
+<section class="banner">
+    <div class="banner__content">
+        <h1 class="banner__title">¡Regístrate ahora!</h1>
+        <p class="banner__description">Únete a nuestra comunidad y disfruta de todos los beneficios.</p>
+        <a href="../Animalandia-web/pages/registro.php" class="banner__button">Registrarse</a>
+    </div>
+</section>
