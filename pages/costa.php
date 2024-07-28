1e6ab045f2b1788219933a4e75bd4f2ac9969b06
Kevin Rivera <shimpo11.kr@gmail.com>
2024-07-28 14:18:33 -0500

    Agregar estilos de banner a la página principal

---
+++ pages/costa.php
@@ -0,0 +1,142 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>🐳 COSTA</title>
+    <link rel="stylesheet" href="../style.css">
+    <link rel="stylesheet" href="../styles.css/pages.css">
+    <link rel="stylesheet" href="../styles.css/card.css">
+    <script src="https://cdn.jsdelivr.net/npm/swiffy-slider@1.6.0/dist/js/swiffy-slider.min.js" crossorigin="anonymous" defer></script>
+    <link href="https://cdn.jsdelivr.net/npm/swiffy-slider@1.6.0/dist/css/swiffy-slider.min.css" rel="stylesheet" crossorigin="anonymous">
+</head>
+
+<body>
+
+    <main>
+        <?php require '../components/header.php'; ?>
+
+        <section class="swiffy-slider slider-nav-animation slider-nav-animation-mycustomanimation">
+            <ul class="slider-container">
+                <li><img src="../assets/img/costa1.png" style="max-width: 100%;height: auto;border-radius:1rem;"></li>
+                <li><img src="../assets/img/costa2.png" style="max-width: 100%;height: auto;border-radius:1rem;"></li>
+                <li><img src="../assets/img/costa3.png" style="max-width: 100%;height: auto;border-radius:1rem;"></li>
+                <li><img src="../assets/img/costa4.png" style="max-width: 100%;height: auto;border-radius:1rem;"></li>
+            </ul>
+
+            <button type="button" class="slider-nav"></button>
+            <button type="button" class="slider-nav slider-nav-next"></button>
+
+            <div class="slider-indicators">
+                <button class="active"></button>
+                <button></button>
+                <button></button>
+            </div>
+            <div class="hero-description">
+                <h1>¡Explora la Fascinante Costa de Ecuador!</h1>
+                <p>La costa de Ecuador es un lugar lleno de aventuras y maravillas. Con playas hermosas, selvas tropicales y una gran variedad de animales, la costa es perfecta para los exploradores jóvenes. ¡Ven y descubre la emoción de la costa ecuatoriana, donde siempre hay algo nuevo por ver!</p>
+            </div>
+        </section>
+
+        <section id="proteger-animales" class="prevencion">
+            <h2>¡Cuidemos a los Animales de la Costa!</h2>
+            <p>La costa ecuatoriana es hogar de muchos animales increíbles que necesitan nuestra ayuda. Aquí tienes algunas maneras divertidas y fáciles de protegerlos:</p>
+            <ul>
+                <li><strong>¡Construye una casita para pájaros!</strong> Ayuda a las aves locales creando un refugio seguro para ellas.</li>
+                <li><strong>Recoge basura</strong> cuando visites la playa. ¡Las tortugas marinas y los cangrejos te lo agradecerán!</li>
+                <li><strong>Usa menos plástico.</strong> Cambia los popotes de plástico por popotes reutilizables para mantener el océano limpio.</li>
+                <li><strong>Infórmate y comparte.</strong> Aprende sobre los animales de la costa y cuéntales a tus amigos y familiares cómo pueden ayudar a protegerlos.</li>
+            </ul>
+            <p>¡Tú puedes ser un héroe para los animales de la costa ecuatoriana! ¡Vamos a cuidar juntos de este maravilloso lugar!</p>
+        </section>
+
+        <section class="section">
+            <div class="title__section">
+                <h2 class="animated-title">¡Descubre los Animales de la Costa!</h2>
+            </div>
+
+            <div class="clasificacion">
+                <div class="card__container">
+                    <img src="../assets/img/costa5.png" alt="Iguana Marina">
+                    <div class="card__content">
+                        <h3 class="card__title">Iguana Marina</h3>
+                        <p class="card__description">
+                            La iguana marina es un reptil que vive en las costas de Ecuador. Con su piel escamosa y su cola larga, la iguana marina es un animal fascinante y misterioso. ¡Ven y conoce a la iguana marina, la reina de los manglares!
+                        </p>
+                        <small class="habitat"><strong>Hábitat:</strong> Manglares, playas y zonas rocosas cercanas al mar.</small>
+                    </div>
+                </div>
+
+                <div class="card__container">
+                    <img src="../assets/img/costa6.png" alt="Pelicano Pardo">
+                    <div class="card__content">
+                        <h3 class="card__title">Pelícano Pardo</h3>
+                        <p class="card__description">
+                            El pelícano pardo es un ave marina que vive en las costas de Ecuador. Con su pico largo y su plumaje oscuro, el pelícano pardo es un animal elegante y majestuoso. ¡Ven y descubre al pelícano pardo, el pescador de los mares!
+                        </p>
+                        <small class="habitat"><strong>Hábitat:</strong> Costas, islas y estuarios.</small>
+                    </div>
+                </div>
+
+                <div class="card__container">
+                    <img src="../assets/img/costa7.png" alt="Armadillo de nueve bandas">
+                    <div class="card__content">
+                        <h3 class="card__title">Armadillo de Nueve Bandas</h3>
+                        <p class="card__description">
+                            Vive en las selvas de la costa ecuatoriana. Con su caparazón duro y sus garras afiladas, el armadillo de nueve bandas es un animal curioso y valiente. ¡Ven y conoce al armadillo de nueve bandas, el explorador de la selva!
+                        </p>
+                        <small class="habitat"><strong>Hábitat:</strong> Bosques secos y áreas semiáridas de la costa.</small>
+                    </div>
+                </div>
+
+                <div class="card__container">
+                    <img src="../assets/img/costa8.png" alt="Caimán de las costa">
+                    <div class="card__content">
+                        <h3 class="card__title">Caimán de la Costa</h3>
+                        <p class="card__description">
+                            El caimán de la costa es un reptil que vive en los ríos y estuarios de la costa ecuatoriana. Con su piel escamosa y sus dientes afilados, el caimán de la costa es un animal poderoso y temible. ¡Ven y descubre al caimán de la costa, el rey de los ríos!
+                        </p>
+                        <small class="habitat"><strong>Hábitat:</strong> Ríos y estuarios de la costa</small>
+                    </div>
+                </div>
+
+                <div class="card__container">
+                    <img src="../assets/img/costa9.png" alt="Tucán de Pico Castaño">
+                    <div class="card__content">
+                        <h3 class="card__title">Tucán de Pico Castaño</h3>
+                        <p class="card__description">
+                            El tucán de pico castaño es un ave colorida que vive en las selvas de la costa ecuatoriana. Con su pico grande y sus plumas brillantes, el tucán de pico castaño es un animal alegre y juguetón. ¡Ven y conoce al tucán de pico castaño, el acróbata de la selva!
+                        </p>
+                        <small class="habitat"><strong>Hábitat:</strong> Bosques húmedos y secos.</small>
+                    </div>
+                </div>
+
+            </div>
+        </section>
+
+        <section class="videos">
+            <div class="title__section">
+                <h2 class="animated-title">¡Descubre las Aventuras de la Costa del Ecuador!</h2>
+            </div>
+            <div class="video__container">
+                <div class="video">
+                    <video src="../assets/video/video-costa4.mp4" title="YouTube video player" controls></video>
+                </div>
+                <div class="video">
+                    <video src="../assets/video/Regiòn COSTA Ecuador para niños.mp4" title="YouTube video player" controls></video>
+                </div>
+                <div class="video">
+                    <video src="../assets/video/video-costa2.mp4" title="YouTube video player" controls></video>
+                </div>
+            </div>
+        </section>
+
+        <?php require '../components/footer.php'; ?>
+    </main>
+
+    <script defer src="../js/index.js"></script>
+
+</body>
+
+</html>
